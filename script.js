eed04ee6463fff18667aabd27bcd0689266d3c1f
Anauthorizednovelistpoeit <104313493+Anauthorizednovelistpoeit@users.noreply.github.com>
2024-10-18 01:43:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ $(document).ready(function () {
   // Scroll up Button script end
 
   // animation Script for display
-  const animationScript = ["Front End Developer", "React.js Developer", "Next.js Developer"];
+  const animationScript = ["B.com Student", "Master in Trading", "And Computer skills"];
 
   // Typing animation script start
   new Typed(".typing", {
